31d66993175c5b23b33588a42fd094bf3307ed31
dbms amigos <ksheer.agrawal@iitgn.ac.in>
2023-04-11 17:03:48 +0530

    css_check

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'surveillancerobot/public')));
 
 let x = 0;
 
